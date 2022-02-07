d25a1b08af75244b14fdcfa56549dc6e92880452
box <823815315@qq.com>
2022-02-07 20:11:00 +0800

    读取文件夹下的excel表并用nodexlsx解析

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,17 @@
+const confJson = require("../Config.json");
+import * as NodeXlsx from "node-xlsx";
+import fs from 'fs';
+import path from "path";
+
 export class Main {
     public run() {
-        console.log(111);
+        fs.readdir(confJson.xlsxDir, (err, files: string[]) => {
+            for (let v of files) {
+                let url = path.resolve(confJson.xlsxDir, v);
+                let result = NodeXlsx.parse(url);
+                debugger
+            }
+        });
     }
 }
 
